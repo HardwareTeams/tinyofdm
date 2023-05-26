6085df747d2f0bf82b32569e1696d0ea2f6481e0
SignalProcessingJobs <hardwareteams@gmail.com>
2023-05-25 22:32:18 -0700

    part2

--- test/test_vis.py
+++ test/test_vis.py
@@ -1,7 +1,8 @@
 import numpy as np 
 import unittest
-from tinyofdm.modem import TxModem
+from tinyofdm.modem import TxModem, RxModem
 from scipy.fft import fft, ifft, fftshift
+from scipy.signal import lfilter
 import matplotlib.pyplot as plt
 
 np.set_printoptions(linewidth=np.inf)
@@ -26,25 +27,98 @@ def plot_fft(data, fs, title_str):
     plt.show(block=False)
     plt.pause(0.001)
     input('Press [Enter] to continue...')
-
+def plot_time(data, title_str):
+    plt.figure()
+    plt.plot(data)
+    plt.xlabel('sample')
+    plt.ylabel('mag')
+    plt.title(title_str)
+    plt.show(block=False)
+    plt.pause(0.001)
+    input('Press [Enter] to continue...')
 class VisTests(unittest.TestCase):
-        def gen_symbol(self):
-            modem = TxModem(fft_size, cp, fs_bb, fs_tx, fc)
-            test_symbol = modem.gen_symbol(test_data)
-            plot_fft(test_symbol[16:], fs_bb, 'tx symbol')
-            assert(True)
+    def gen_symbol(self):
+        modem = TxModem(fft_size, cp, fs_bb, fs_tx, fc)
+        test_symbol = modem.gen_symbol(test_data)
+        plot_fft(test_symbol[16:], fs_bb, 'tx symbol')
+        assert(True)
 
-        def interp(self):
-            modem = TxModem(fft_size, cp, fs_bb, fs_tx, fc)
-            test_symbol = modem.gen_symbol(test_data)
-            tx_interp = modem.interpolate_to_fstx(test_symbol[-64:])
-            plot_fft(tx_interp, fs_tx, 'tx symbol upsampled')
+    def interp(self):
+        modem = TxModem(fft_size, cp, fs_bb, fs_tx, fc)
+        test_symbol = modem.gen_symbol(test_data)
+        tx_interp = modem.interpolate_to_fstx(test_symbol[-64:])
+        plot_fft(tx_interp, fs_tx, 'tx symbol upsampled')
+    
+    def freq_shift(self):
+        modem = TxModem(fft_size, cp, fs_bb, fs_tx, fc)
+        test_symbol = modem.gen_symbol(test_data)
+        tx_interp = modem.interpolate_to_fstx(test_symbol[-64:])
+        tx_shift = modem.freq_shift(tx_interp)
+        plot_fft(tx_shift, fs_tx, 'tx freq. shifted')
+
+class LoopBack(unittest.TestCase):
+     def loop_back(self):
+        test_chan = np.array([.8, .0001, .0001, .001, .1+.1j])
+        tx = TxModem(fft_size, cp, fs_bb, fs_tx, fc)
+        rx = RxModem(fft_size, cp, fs_bb, fs_tx, fc)
+
+        bpsk_seq = np.zeros(tx.fft_size, 'complex')
+        bpsk_idx = tx.s2u(np.arange(-26, 27, 1))
+
+        bpsk_seq[(bpsk_idx)] = [-.7-.7j,.7+.7j, .7-.7j,-.7+.7j, 1, 1, 1, 1,
+                                -1, 1, 1,  1,  1,  1,  1, -1, 
+                                -1, 1, 1, -1,  1, -1,  1,  1, 
+                                 1, 1, 0,  1, -1, -1,  1,  1, 
+                                -1, 1, -1, 1, -1, -1, -1, -1, 
+                                -1, 1,  1,-1, -1,  1, -1,  1, 
+                                -1, 1, .4, -1,    .5+.5j]
+
+        bpsk_test_seq = tx.gen_symbol(bpsk_seq)
+
+        tx_all = np.concatenate((np.zeros(500,'complex'),\
+                                tx.short_preamble,\
+                                tx.long_preamble,\
+                                bpsk_test_seq,\
+                                np.zeros(500, 'complex')))
+
+        tx_data = tx.transmit(tx_all, False)
+
+        rx_raw = lfilter(tx.upsample(test_chan, tx.os_factor), [1.], tx_data) + \
+                 np.random.normal(0, 0.05, len(tx_data))
         
-        def freq_shift(self):
-            modem = TxModem(fft_size, cp, fs_bb, fs_tx, fc)
-            test_symbol = modem.gen_symbol(test_data)
-            tx_interp = modem.interpolate_to_fstx(test_symbol[-64:])
-            tx_shift = modem.freq_shift(tx_interp)
-            plot_fft(tx_shift, fs_tx, 'tx freq. shifted')
+        # downconvert
+        rx_dc = rx.freq_shift(rx_raw)
+        rx_iq = rx.decimate(rx_dc)
+
+        #detection chain
+        rx_st_det, found_start, rx_start_st = rx.detect_symbol(rx_iq, 16)
+        rx_lt_det, rx_start_lt = rx.ofdm_start(rx_iq)
+
+        # for now calculating offsets, todo:cleanup
+        rx_start_lt += 32
+        rx_data_start = rx_start_lt + (160-32) + 16
+        lt_received = rx_iq[rx_start_lt:rx_start_lt+128]
+
+        # calculate channel response
+        LTS0 = fft(lt_received[:rx.fft_size])
+        LTS1 = fft(lt_received[-rx.fft_size:])
+        H = (0.5 * (LTS0+LTS1))/rx.lt_ifft_in
+
+        plt.figure
+        plt.plot(fftshift(20*np.log10(np.abs(1/H))))
+        plt.plot(fftshift(20*np.log10(np.abs(fft(test_chan,64)))))
+        plt.title('channel estimation')
+        plt.show()
+
+        received_data_symbol = rx_iq[rx_data_start:rx_data_start+rx.fft_size]
+        received_data = fft(received_data_symbol) * 1/H
+
+        plt.figure
+        plt.scatter(np.real(received_data), np.imag(received_data))
+        plt.title('iq')
+        plt.show()
+
+        print('received data')
+        print(np.round(fftshift(received_data),2))
         
        
--- tinyofdm/modem.py
+++ tinyofdm/modem.py
@@ -26,6 +26,60 @@ class OfdmModem:
         self.fft_size = fft_size # hardcode for now
         self.os_factor = int(self.fs_tx/self.fs_bb)
 
+        """
+        short training (st) sequence/preamble
+        """
+        st_seq = np.zeros((self.fft_size), 'complex')
+        # ifft in scipy is [0] index is DC
+        # 1:nfft/2 is + frequencies
+        # nfft/2+1:nfft are - frequencies
+
+        # these are the carriers we will fill for the st 
+        sts_idx_pos = self.s2u(np.array([-24, -16, -4, 12, 16, 20, 24]))
+        sts_idx_neg = self.s2u(np.array([-20, -12, -8, 4, 8]))
+        st_seq[(sts_idx_pos)] = 1+1j
+        st_seq[(sts_idx_neg)] = -1-1j
+        self.st_ifft_in = st_seq
+        self.st_ifft_out = ifft(np.sqrt(13/6)*st_seq)
+        sym_temp = self.add_cylic_prefix(self.st_ifft_out)
+        self.short_preamble = np.concatenate((sym_temp, sym_temp))
+
+        """
+        long training (lt) sequence/preamble
+        """
+        lt_seq = np.zeros((self.fft_size), 'complex')
+        lt_idx = self.s2u(np.arange(-26, 27,1))
+        lt_seq[(lt_idx)] = [1, 1, -1, -1, 1,  1, -1, 1,
+                           -1, 1,  1,  1, 1,  1,  1,-1, 
+                           -1, 1,  1, -1, 1, -1,  1, 1, 
+                            1, 1,  0,  1,-1, -1,  1, 1, 
+                           -1, 1, -1,  1,-1, -1, -1,-1, 
+                           -1, 1,  1, -1,-1,  1, -1, 1, 
+                           -1, 1,  1,  1, 1]
+        self.lt_ifft_in = lt_seq
+        self.lt_ifft_out = ifft(lt_seq)
+        self.long_preamble = np.concatenate((self.add_cylic_prefix(self.lt_ifft_out,32), self.lt_ifft_out))
+
+
+    def s2u(self,list):
+        # convert negative fft carriers to positive for scipy
+        neg_idxs = np.where(list<0)[0]
+        list[neg_idxs] = list[neg_idxs] + self.fft_size
+        return list
+
+    def add_cylic_prefix(self,data,cp=None):
+        if(cp is None):
+            cp = self.cp_len
+        return np.concatenate((data[-cp:], data))
+    
+    def upsample(self, data, I=None):
+        if(I is None):
+            I = int(self.fs_tx/self.fs_bb)
+        temp = np.vstack((data, np.zeros((I-1, len(data)), 'complex')))
+        sz = np.shape(temp)
+        udata = np.reshape(temp.T, (1,sz[0]*sz[1]))
+        return udata[0].T
+       
 class TxModem(OfdmModem):
     def __init__(self, fft_size, cp_len, fs_bb, fs_tx, fc):
         OfdmModem.__init__(self, fft_size, cp_len, fs_bb, fs_tx, fc)
@@ -44,8 +98,55 @@ class TxModem(OfdmModem):
     def freq_shift(self, data):
         return np.real(data * np.exp(1j * 2 * np.pi * self.fc/self.fs_tx * np.arange(len(data))))
     
-    def transmit(self, data):
-        data_u = self.interpolate_to_fstx(data)
-        data_fc = self.freq_shift(data_u)
+    def transmit(self, data, audio=False):
+        tx = self.freq_shift(self.interpolate_to_fstx(data))
+        #play data out of soundcard, also return the data
+        #so that we can do loop back tests
+        tx /= np.max(np.abs(tx)) # scale data
+        if(audio):
+            sd.play(np.real(tx), self.fs_tx)
+            sd.wait()
+        return tx
+
+class RxModem(OfdmModem):
+    def __init__(self, fft_size, cp_len, fs_bb, fs_tx, fc):
+        OfdmModem.__init__(self, fft_size, cp_len, fs_bb, fs_tx, fc)
+    
+    def rm_cyclic_prefix(self, data):
+       return data[self.cp_len:]
+    
+    def freq_shift(self, data):
+        return data * np.exp(-1j * 2 * np.pi * self.fc/self.fs_tx * np.arange(len(data)))
+    
+    def decimate(self, data):
+        return resample(data, int(len(data)/self.os_factor))
+    
+    def detect_symbol(self, data, L):
+        dout = np.zeros(len(data))
+        for ii in range(len(data)-2*L):
+            psum = np.dot(data[ii+L:ii+2*L], np.conj(data[ii:ii+L]))
+            rsum = np.sum(np.square(np.abs(data[ii+L:ii+2*L])))
+            dout[ii] = np.square(np.abs(psum))/np.square(rsum) * (rsum > 0.05)
+        idxs = np.where(dout>=0.95)[0]
+        return dout, True if idxs.size > 0 else False, idxs[0]
+    
+    def ofdm_start(self,data):
+        L = self.fft_size
+        y = np.zeros(len(data)-L)
+        for ii in range(len(data)-L):
+            y[ii] = np.abs(np.dot(data[ii:ii+L], np.conj(self.lt_ifft_out)))
+        indexes_of_detect = np.where(y>=0.85*np.max(y))[0]
+        return y, indexes_of_detect[0]-32
+    
+    def receive(self, seconds):
+        rx = np.zeros((int(self.fs_tx*seconds), 1))
+        sd.rec(samplerate=self.fs_tx, channel=1, out=rx)
+        sd.wait()
+        return np.reshape(rx,(len(rx)))
+
+   
+
+    
+
 
 
